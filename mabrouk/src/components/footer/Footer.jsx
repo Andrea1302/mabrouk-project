fbf031f3b89e3cfe2444a2d3cfe3d76b8cef8369
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2023-03-21 11:50:39 +0100

    create footer

---
+++ mabrouk/src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        background: "#000",
+        height: "40vh",
+        paddingTop: "10vh",
+        color: "#fff",
+      }}
+      id="contact-us"
+    >
+      Contatti
+    </footer>
+  );
+};
+
+export default Footer;
